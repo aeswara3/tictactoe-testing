b902ad52637dafcd4397db1254fae92e39450dff
anirudheswara <aeswara2@illinois.edu>
2020-02-04 14:11:51 -0600

    Tests Complete

--- src/main/java/student/tictactoe/TicTacToe.java
+++ src/main/java/student/tictactoe/TicTacToe.java
@@ -3,7 +3,5 @@ package student.tictactoe;
 import student.tictactoe.Evaluation;
 
 public class TicTacToe {
-    public static Evaluation evaluateBoard(String boardState) {
-        return Evaluation.NoWinner;
-    }
+    
 }
--- src/test/java/student/tictactoe/TicTacToeTest.java
+++ src/test/java/student/tictactoe/TicTacToeTest.java
@@ -9,15 +9,67 @@ import static org.junit.Assert.*;
 
 public class TicTacToeTest {
     @Test
-    public void simpleNoWinnerBoard() throws Exception {
-        assertEquals(Evaluation.NoWinner, TicTacToe.evaluateBoard("O...X.X.."));
+    public void invalidInput() throws Exception {
+        assertEquals(Evaluation.InvalidInput, TicTacToe.evaluateBoard("abcxyz"));
     }
+
     @Test
-    public void simpleXWins() throws Exception {
+    public void tooManyX() throws Exception {
+        assertEquals(Evaluation.UnreachableState, TicTacToe.evaluateBoard("XX.....XO"));
+    }
+
+    @Test
+    public void tooManyO() throws Exception {
+        assertEquals(Evaluation.UnreachableState, TicTacToe.evaluateBoard("OXO...OX."));
+    }
+
+    @Test
+    public void TwoWinners() throws Exception {
+        assertEquals(Evaluation.UnreachableState, TicTacToe.evaluateBoard("XXX...OOO"));
+    }
+
+    @Test
+    public void OTurnAfterXWins() throws Exception {
+        assertEquals(Evaluation.UnreachableState, TicTacToe.evaluateBoard("XXX.OO..O"));
+    }
+
+    @Test
+    public void XTurnAfterOWins() throws Exception {
+        assertEquals(Evaluation.UnreachableState, TicTacToe.evaluateBoard("XXOXXO..O"));
+    }
+
+    @Test
+    public void verticalWinnerXWins() throws Exception {
+        assertEquals(Evaluation.Xwins, TicTacToe.evaluateBoard("OXO.X.OXX"));
+    }
+
+    @Test
+    public void verticalWinnerOWins() throws Exception {
+        assertEquals(Evaluation.Owins, TicTacToe.evaluateBoard("OXXO..OX."));
+    }
+
+    @Test
+    public void horizontalWinnerXWins() throws Exception {
+        assertEquals(Evaluation.Owins, TicTacToe.evaluateBoard("XXXOO...."));
+    }
+
+    @Test
+    public void horizontalWinnerOWins() throws Exception {
+        assertEquals(Evaluation.Xwins, TicTacToe.evaluateBoard("..XOOOXX."));
+    }
+
+    @Test
+    public void diagonalWinnerXWins() throws Exception {
         assertEquals(Evaluation.Xwins, TicTacToe.evaluateBoard("XO.OX...X"));
     }
+
     @Test
-    public void simpleOWins() throws Exception {
-        assertEquals(Evaluation.Owins, TicTacToe.evaluateBoard("XOXXO..O."));
+    public void diagonalWinnerOWins() throws Exception {
+        assertEquals(Evaluation.Owins, TicTacToe.evaluateBoard("XXOXO.O.."));
+    }
+
+    @Test
+    public void NoWinner() throws Exception {
+        assertEquals(Evaluation.NoWinner, TicTacToe.evaluateBoard("XO.XO...."));
     }
 }
